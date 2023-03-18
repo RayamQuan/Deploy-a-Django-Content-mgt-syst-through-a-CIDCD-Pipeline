9d37a707ec7d426befa4b0bce74b84745494cca4
Dangana Rayam Inebhari <96420808+RayamQuan@users.noreply.github.com>
2023-03-18 17:44:38 +0100

    Update run_kubernetes.sh

--- django_project/run_kubernetes.sh
+++ django_project/run_kubernetes.sh
@@ -7,7 +7,7 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=maiempire/django_project-app
+dockerpath=virgoinvincible/django_project-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
